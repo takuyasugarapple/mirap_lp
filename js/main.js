5ba5e33dfe8448d7f09526b1b0db5338b3320205
takuyasugarapple <takuyasugarapple@gmail.com>
2019-02-19 23:52:54 +0900

    Update style.css
    一旦コミット

--- js/main.js
+++ js/main.js
@@ -52,7 +52,7 @@ $(function () {
   var scrollAnimationElm = document.querySelectorAll('.sa');
   var scrollAnimationFunc = function () {
     for (var i = 0; i < scrollAnimationElm.length; i++) {
-      var triggerMargin = 700;
+      var triggerMargin = 500;
       if (window.innerHeight > scrollAnimationElm[i].getBoundingClientRect().top + triggerMargin) {
         scrollAnimationElm[i].classList.add('show');
       }
